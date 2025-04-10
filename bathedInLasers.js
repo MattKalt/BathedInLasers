da7c628a2357d9ed0ff2b06fef58e4ed1361e5df
Matt Kalt <93488504+MattKalt@users.noreply.github.com>
2025-04-09 23:03:53 -0400

    added delay on saws and cool stereo glitch

--- bathedInLasers.js
+++ bathedInLasers.js
@@ -231,7 +231,11 @@ G1=pn=>lp(lp2(cl(hp(og(p*t,1-pn),.02)*seq(ld,16,t,1)),.7),min(t/1e5,.7)),
 G2=pn=>lp2(cl(hp(cl(lp(og(p*t,pn),.5)*(seq(ld,16,t,1)+.2)/9),.2)),min(t/1e6,.1)),
 
 sw=x=>x%1+x*.99%1+x*1.01%1,
-SW=(pn,t)=>hp(sw(mseq(pn?l1:l2,10,t,4-pn)*(6+pn/8)/97),.25)*.6*seq(lv,16),
+SW=(pn,t)=>hp(sw(mseq(pn?l1:l2,10,t,t>>13==7?0:4-pn)*(6+pn/8)/97),.25)*.6*seq(lv,16),
+//SW=(pn,t)=>hp(sw(mseq(pn?l1:l2,10,t,4-pn)*(6+pn/8)/97),.25)*.6*seq(lv,16),
+//SWs=pn=>t>>15>1?SW(pn,t):SW(pn,t)*.9+SW(pn,t-8192)*.6,
+SWs=pn=>t>>13>7?SW(pn,t)*.8+SW(pn,t-512)*.4:lp(SW(pn,t)*.9+SW(pn,t-8e3)*.6,min(t/1e5,1)), //uses different amount of memory, causing stereo glitch that sounds COOL AF
+
 
 l3m=pn=>mseq(l3,11,t,pn)|mseq(l3,11,t,pn)/2,
 L3=pn=>cl(sw(l3m(pn)*(6+pn/8)/97)*seq(lv,16)*64)*(t>>15>1?.1:0),
@@ -239,15 +243,15 @@ L3=pn=>cl(sw(l3m(pn)*(6+pn/8)/97)*seq(lv,16)*64)*(t>>15>1?.1:0),
 BS1 = x => x&0 + x&192,
 BS = x => cl(((BS1(x)/2 + hp(BS1(x),.1)*(-t>>6&8))%99)/64) * seq(bsv,10)/8,
 
-K = cl(((sin(sqrt(6*(t%1024)))*127+(t/2&127))*bt(drk,10,1)**.125)/16)*(t>>9>90?1:0),
-SN = bt([s],9)*bt(drs,10,2,.4)*(t>>9>64?1:0),
+K = cl(((sin(sqrt(6*(t%1024)))*127+(t/2&127))*bt(drk,10,1)**.125)/16)*(t>>9>72?1:0),
+SN = bt([s],9)*bt(drs,10,2,.4)*(t>>9>63?1:0),
 H = bt([h],10,3*seq(hhv,16),1),
 
 Mix = pan => (
 
-BS(mseq(bas,10))*.75 + SW(pan) + L3(pan)*.8 + hp(L3(pan),.4)*2 + K + SN + H +
+BS(mseq(bas,10))*.6 + SWs(pan) + L3(pan)*.9 + hp(L3(pan),.4)*2 + K + SN + H +
 
-G1(pan) * (.1 + pan/8) + G2(pan) * (.7 - pan/2)
+G1(pan) * (.05 + pan/8) + G2(pan) * (.6 - pan/2)
 
 ),
 
